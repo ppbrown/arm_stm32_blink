719fdbec245d02fe058c72c151a3773470d974ef
Philip Brown <phil@bolthole.com>
2023-02-07 20:20:29 -0800

    Change the f4 prog and regs inc to something closer to bare hardware, instead of functionally designed to serve multiple hardware

--- blink_stm32f4.s
+++ blink_stm32f4.s
@@ -7,6 +7,94 @@
 
 	.include "regs_stm32f4_inc.s"
 
-	// The actual program
-	.include "blink_inc.s"
 
+	.equ	DELAY_INTERVAL,	0x186004
+
+
+
+	// Export functions so they can be called from other file
+	.global asm_rcc_enable
+	.global asm_gpio_set_mode_output
+	.global asm_gpio_toggle
+	.global reset_handler
+	.global main
+
+
+	// enable clock for GPIOA and GPIOC
+asm_rcc_enable:	
+	ldr	r1, =RCC_AHB1ENR
+	mov	r2, #RCC_GPIOA_EN
+	orr	r2, #RCC_GPIOC_EN
+	
+	ldr	r3, [r1]	// Load in values for all clocks
+	orr	r3, r2		// just OR, since we KNOW we are adding bits only
+	str	r3, [r1]
+	bx	lr
+
+	// Set the mode on the pins we want, to mode=output
+	// be ugly and ignore prior settings.
+	// If we were nice we would merge with values for other pins
+asm_gpio_set_A5_output:
+	ldr	r0,=GPIOA_MODER
+	
+	mov 	r1, #GPIO_MODE_OUTPUT
+	lsl	r1, #(5*2)	// "pin5, x2 bits"
+	str	r1, [r0]
+
+	
+	ldr	r0,=GPIOA_PUPDR
+	mov 	r1, #GPIO_MODE_OUTPUT
+	lsl	r1, #(5*2)	// "pin5, x2 bits"
+	str	r1, [r0]
+	
+
+
+	bx	lr
+	
+
+	
+	
+	// We specifically only toggle pin5 for GPIOA
+asm_gpio_toggle:
+	ldr	r3, =GPIOA_ODR  // has port addr
+	ldr     r1, [r3]	// has port addr contents
+
+	ldr     r2, [r3]	// has port same
+	mvns    r2, r2		// has ~(port)
+	and.w   r1, r1, #GPIO5		// port & GPIO5
+	lsls    r1, r1, #16		// has (port & GPIO5) << 16
+
+	and.w   r2, r2, #GPIO5		// has ~port & GPIO5
+
+	orrs    r1, r2			// r1 has all the goodies
+
+	ldr	r3, =GPIOA_BSRR
+	str	r1, [r3]
+
+	bx      lr
+
+
+main:
+	bl	asm_rcc_enable
+	bl	asm_gpio_set_A5_output
+loop1:
+	ldr	r0, =DELAY_INTERVAL
+loop2:
+	nop
+	sub	r0, #1
+	cmp	r0, #0
+	bge	loop2
+
+	bl	asm_gpio_toggle
+
+	b	loop1
+	
+	
+
+	/* This is called on first start, and if reset button pushed */
+	/* In theory, would clear out stack,etc. */
+reset_handler:
+	
+	b	main
+
+	
--- regs_stm32f4_inc.s
+++ regs_stm32f4_inc.s
@@ -9,6 +9,9 @@
 	 *
 	\********************************************************************/
 
+	//    https://www.st.com/resource/en/reference_manual/rm0368-stm32f401xbc-and-stm32f401xde-advanced-armbased-32bit-mcus-stmicroelectronics.pdf
+
+
 	/* Future note: according to the board docs,
 	 * "the user button is connected to the I/O PC13 (pin 2)  ..."
 	 * aka GPIOC13, 
@@ -33,18 +36,11 @@
 	// #define _REG_BIT(base, bit)		(((base) << 5) + (bit))
 	// RCC_GPIOA       = _REG_BIT(0x30, 0),
 
-	.equ	RCC_GPIOA, AHB1ENR_OFFSET <<5
+	.equ	RCC_GPIOA_EN, (1 <<0)
+	.equ	RCC_GPIOB_EN, (1 <<1)
+	.equ	RCC_GPIOC_EN, (1 <<2)
 
-	// #define _RCC_REG(i)             MMIO32(RCC_BASE + ((i) >> 5))
-	// #define _RCC_BIT(i)             (1 << ((i) & 0x1f))
-
-	// rcc_periph_clock_enable(RCC_GPIOA);
-        // _RCC_REG(RCC_GPIOA) |= _RCC_BIT(RCC_GPIOA);
-
-	.equ	RCC_REG_GPIOA, RCC_BASE + AHB1ENR_OFFSET
-	.equ	RCC_BIT_GPIOA, 1 << ((RCC_GPIOA) & 0x1f)
-
-
+	
 
 	/*************************************************************************/
 	/*   ......   GPIO Section .......                                       */
@@ -52,46 +48,58 @@
 	
 	.equ	GPIO_BASE, PERIPH_BASE_AHB1
 
-	.equ	GPIOA, GPIO_BASE + 0x000	// GPIO port A (also "GPIO_PORT_A_BASE")
-	.equ	GPIOB, GPIO_BASE + 0x400	// GPIO port B (also "Base")
-	.equ	GPIOC, GPIO_BASE + 0x800	// GPIO port C (also "Base")
-	.equ	GPIOD, GPIO_BASE + 0xC00	// GPIO port D (also "Base")
-	.equ	GPIOE, GPIO_BASE + 0x1000	// GPIO port E (also "Base")
-	.equ	GPIOF, GPIO_BASE + 0x1400	// GPIO port F (also "Base")
+	.equ	GPIOA_BASE, GPIO_BASE + 0x000	// GPIO port A (also "GPIO_PORT_A_BASE")
+	.equ	GPIOB_BASE, GPIO_BASE + 0x400	// GPIO port B (also "Base")
+	.equ	GPIOC_BASE, GPIO_BASE + 0x800	// GPIO port C (also "Base")
+	.equ	GPIOD_BASE, GPIO_BASE + 0xC00	// GPIO port D (also "Base")
+	.equ	GPIOE_BASE, GPIO_BASE + 0x1000	// GPIO port E (also "Base")
+	.equ	GPIOF_BASE, GPIO_BASE + 0x1400	// GPIO port F (also "Base")
 
 	.equ	GPIO5,	(1 << 5)	// GPIO ports, pin5
 	.equ	GPIO13,	(1 << 13)	// GPIO ports, pin13
 
-	.equ	GPIO_MODE_INUT,	  0x0
+	// These have to be shifted over to the appropriate slots in the
+	// approprate GPIOx MODE register
+	// GPIOA pin0, is bits 0 and1
+	// GPIOA pin1, is bits 2 and 3, etc.
+
+	.equ	GPIO_MODE_INPUT,  0x0
 	.equ	GPIO_MODE_OUTPUT, 0x1
-	.equ	GPIO_MODE_AF,     0x2
+	.equ	GPIO_MODE_ALTF,   0x2
 	.equ	GPIO_MODE_ANALOG, 0x3
-	
+
+
+	// Like the MODER register, these have to be left-shifted to match
+	// which pin we are talking about
 	.equ	GPIO_PUPD_NONE,     0x0
 	.equ	GPIO_PUPD_PULLUP,   0x1
 	.equ	GPIO_PUPD_PULLDOWN, 0x2
 
 	
-	.equ	GPIO_PUPD_MASK_PIN5, (0x3 << (2 * (5)))
-	.equ	GPIO_MODE_MASK_PIN5, (0x3 << (2 * (5)))
-	.equ	GPIO_MODE_OUTPUT_PIN5, (GPIO_MODE_OUTPUT) << (2 * (5))
 
 
 //GPIO-A control registers
-	.equ	GPIOA_MODER,	GPIOA + 0x00	//set GPIO pin mode as Input/Output/Analog
-	.equ	GPIOA_OTYPER,	GPIOA + 0x04 	//Set GPIO pin type as push-pull or open drain
-	.equ	GPIOA_OSPEEDR,	GPIOA + 0x08	//Set GPIO pin switching speed
-	.equ	GPIOA_PUPDR,	GPIOA + 0x0C	//Set GPIO pin pull-up/pull-down
-	.equ	GPIOA_ODR,	GPIOA + 0x14	//GPIO pin output data
-	.equ	GPIOA_BSRR,	GPIOA + 0x18	// Bit Set/Reset
+	.equ	GPIOA_MODER,	GPIOA_BASE + 0x00	// GPIOA "mode register"
+	.equ	GPIOA_OTYPER,	GPIOA_BASE + 0x04 	//Set GPIO pin type as push-pull or open drain
+	.equ	GPIOA_OSPEEDR,	GPIOA_BASE + 0x08	//Set GPIO pin switching speed
+	.equ	GPIOA_PUPDR,	GPIOA_BASE + 0x0C	//Set GPIO pin pull-up/pull-down
+	.equ	GPIOA_IDR,	GPIOA_BASE + 0x10	//GPIO pin input data
+	.equ	GPIOA_ODR,	GPIOA_BASE + 0x14	//GPIO pin output data
+	.equ	GPIOA_BSRR,	GPIOA_BASE + 0x18	// Bit Set/Reset
 
 
 //GPIO-B control registers
-	.equ	GPIOB_MODER,	GPIOB + 0x00	//set GPIO pin mode as Input/Output/Analog
+	.equ	GPIOB_MODER,	GPIOB_BASE + 0x00	//set GPIO pin mode as Input/Output/Analog
 	// ...
 //GPIO-C control registers
-	.equ	GPIOC_MODER,	GPIOC + 0x00	//set GPIO pin mode as Input/Output/Analog
-	// ...
+	.equ	GPIOC_MODER,	GPIOC_BASE + 0x00	//set GPIO pin mode as Input/Output/Analog
+	.equ	GPIOC_OTYPER,	GPIOC_BASE + 0x04 	//Set GPIO pin type as push-pull or open drain
+	.equ	GPIOC_OSPEEDR,	GPIOC_BASE + 0x08	//Set GPIO pin switching speed
+	.equ	GPIOC_PUPDR,	GPIOC_BASE + 0x0C	//Set GPIO pin pull-up/pull-down
+	.equ	GPIOC_IDR,	GPIOC_BASE + 0x1C	//Set GPIO pin pull-up/pull-down
+	.equ	GPIOC_ODR,	GPIOC_BASE + 0x14	//GPIO pin output data
+	.equ	GPIOC_BSRR,	GPIOC_BASE + 0x18	// Bit Set/Reset
+
 	
 //GPIO-D control registers
 	.equ	GPIOD_MODER,	GPIOD + 0x00	//set GPIO pin mode as Input/Output/Analog
